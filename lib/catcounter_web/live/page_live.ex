4a099bc52654ad5d4288ca4d1fbb90fd6d52a844
MmmarRTHa <m27842n@outlook.com>
2023-03-15 22:58:21 -0700

    - update: main - added cat counter with phoenix sockets and TailwindsCSS UI.

--- lib/catcounter_web/live/page_live.ex
+++ lib/catcounter_web/live/page_live.ex
@@ -1,3 +1,27 @@
 defmodule CatcounterWeb.PageLive do
   use CatcounterWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok,
+    socket
+    |> assign(number: 0)}
+  end
+
+  def handle_event("increment", _params, socket) do
+    {:noreply,
+    socket
+    |> update(:number, &(&1 + 1))}
+  end
+
+  def handle_event("decrement", _params, socket) do
+    {:noreply,
+    socket
+    |> update(:number, &max(0, &1 - 1))}
+  end
+
+  def handle_event("reset", _params, socket) do
+    {:noreply,
+    socket
+    |> assign(number: 0)}
+  end
 end
--- test/catcounter_web/live/page_live_test.exs
+++ test/catcounter_web/live/page_live_test.exs
@@ -10,18 +10,18 @@ defmodule CatcounterWeb.PageLiveTest do
 
 	test "increment and decrement events", %{conn: conn} do
 		{:ok, page_live, _disconnected_html} = live(conn, "/")
-		assert render_click(page_live, :inc, %{}) =~ "1"
-		assert render_click(page_live, :inc, %{}) =~ "2"
-		assert render_click(page_live, :inc, %{}) =~ "3"
-		assert render_click(page_live, :dec, %{}) =~ "2"
-		assert render_click(page_live, :dec, %{}) =~ "1"
-		assert render_click(page_live, :dec, %{}) =~ "0"
+		assert render_click(page_live, :increment, %{}) =~ "1"
+		assert render_click(page_live, :increment, %{}) =~ "2"
+		assert render_click(page_live, :increment, %{}) =~ "3"
+		assert render_click(page_live, :decrement, %{}) =~ "2"
+		assert render_click(page_live, :decrement, %{}) =~ "1"
+		assert render_click(page_live, :decrement, %{}) =~ "0"
 	end
 
 	test "clear event", %{conn: conn} do
 		{:ok, page_live, _html} = live(conn, "/")
-		assert render_click(page_live, :inc, %{}) =~ "1"
-		assert render_click(page_live, :clear, %{}) =~ "0"
+		assert render_click(page_live, :increment, %{}) =~ "1"
+		assert render_click(page_live, :reset, %{}) =~ "0"
 	end
 
 end
